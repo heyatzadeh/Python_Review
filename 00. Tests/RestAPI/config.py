83d6ab1f1cb571f62bb5f514c82c59ad9dab339f
Hossein <hheyatzadeh@gmail.com>
2023-03-16 18:25:57 +0330

    Fix gitignore

--- 00. Tests/RestAPI/config.py
+++
@@ -1,6 +0,0 @@
-yelp_api_key = "jRx4ZI5HJ3cnTSMYJMGK3UzCdtGCQgfwnNuyuIKqLwNSP_7_mG7jwdz21LVAs55T4qFxCkrgDucz2YFu" \
-               "-mJFLbnd_90N6lM_xPmjguH3CAQIZIulXybLoqmfxt0SZHYx"
-
-twilio_SID = "ACf42660d75c27281385a6466c08d2d3aa"
-
-twilio_Auth_Token = "0660df4aacdd948a84d3fdaf6f3cccb1"
