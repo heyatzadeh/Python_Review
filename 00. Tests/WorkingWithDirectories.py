11f227b1610f20ef1b1ad7d4fb51053fd92f41aa
Hossein <hheyatzadeh@gmail.com>
2023-05-26 10:28:39 +0330

    Refactoring WorkingWithDirectories

--- 00. Tests/WorkingWithDirectories.py
+++ 00. Tests/WorkingWithDirectories.py
@@ -1,8 +1,9 @@
+# number = 9
 import os
 import shutil
 
 # directory = os.curdir
-directory = r"D:\Projects\Python"
+directory = r"F:\TV Shows\Want to Watch\Fullmetal Alchemist Brotherhood"
 
 # for root, dirs, files in os.walk(directory):
 #     for name in dirs:
@@ -13,16 +14,16 @@ directory = r"D:\Projects\Python"
 
 
 # get all files in directory and then rename them
-# for filename in os.listdir(directory):
-#     filename_path = os.path.join(directory, filename)
-#     extension = os.path.splitext(filename)[1]
-#     new_filename = filename.replace(".FilmKio", "") + extension
-#     new_filename_path = os.path.join(directory, new_filename)
-#     shutil.move(filename_path, new_filename_path)
+for filename in os.listdir(directory):
+    filename_path = os.path.join(directory, filename)
+    # extension = os.path.splitext(filename)[1]
+    new_filename_path = os.path.join(directory, filename.replace(".AvaMovie", ""))
+    print('Old file: ' + filename_path)
+    print('New file: ' + new_filename_path)
+    os.rename(filename_path, new_filename_path)
 
-number = 9
-for file in os.listdir(directory):
-    if file.startswith(str(number).zfill(2)):
-        shutil.move(os.path.join(directory, file),
-                    os.path.join(directory, file.replace(str(number).zfill(2), str(number - 1).zfill(2))))
-        number += 1
+# for file in os.listdir(directory):
+#     if file.startswith(str(number).zfill(2)):
+#         shutil.move(os.path.join(directory, file),
+#                     os.path.join(directory, file.replace(str(number).zfill(2), str(number - 1).zfill(2))))
+#         number += 1
