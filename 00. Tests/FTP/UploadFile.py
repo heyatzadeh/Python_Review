9023c8f64d6e45b6ab2ebd5a1c3d747aadd9605d
Hossein Heyatzadeh <H.Heyatzadeh@sadad.co.ir>
2023-06-07 17:06:05 +0330

    Refactor upload to FTP server

--- 00. Tests/FTP/UploadFile.py
+++ 00. Tests/FTP/UploadFile.py
@@ -1,15 +1,14 @@
-import FTPOverSSL
 import config
-import datetime
+from FTPOverSSL import ImplicitFTP_TLS
+from datetime import datetime
 
-file = open('./logs.zip', "rb")
-with FTPOverSSL.ImplicitFTP_TLS() as ftps:
-    for i in range(1000):
-        time = datetime.datetime.now()
-        ftps.connect(host=config.url, port=config.port)
-        ftps.login(user=config.user, passwd=config.password)
-        ftps.prot_p()
-        ftps.storbinary(f'STOR /in_file/79/test/I94430003-D20230423-T{time.strftime("%H-%M-%S-%f")}Test.zip', file)
-        print(f'Number: {i} successful I94430003-D20230423-T{time.strftime("%H-%M-%S-%f")}Test.zip')
-
-file.close()
+with ImplicitFTP_TLS() as session:
+    session.connect(host=config.url, port=config.port)
+    session.login(user=config.user, passwd=config.password)
+    session.prot_c()
+    while True:
+        file = open('./logs.zip', "rb")
+        session.storbinary(f'STOR /in_file/79/test/I10001005-{datetime.now().strftime("D%Y%m%d-T%H-%M-%S-%f")}.zip',
+                           file)
+        print('successfully uploaded')
+        file.close()
