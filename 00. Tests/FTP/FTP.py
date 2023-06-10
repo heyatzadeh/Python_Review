f0b141b614959d56ee9ca66c2d4bfaa37182eb6c
Hossein Heyatzadeh <H.Heyatzadeh@sadad.co.ir>
2023-06-10 14:44:48 +0330

    Remove for loop

--- 00. Tests/FTP/FTP.py
+++ 00. Tests/FTP/FTP.py
@@ -4,26 +4,24 @@ import FTPOverSSL
 import CSVReader
 import config
 
-for i in range(200):
-    time = datetime.now()
+time = datetime.now()
 
-    with FTPOverSSL.ImplicitFTP_TLS() as ftps:
-        ftps.connect(host=config.url, port=config.port)
-        ftps.login(user=config.user, passwd=config.password)
-        ftps.prot_p()
-        # ftps.set_pasv(True)
-        ftps.cwd(f'/in_file/00/logs/btm/{time.strftime("%Y/%m/%Y%m09")}')
+with FTPOverSSL.ImplicitFTP_TLS() as ftps:
+    ftps.connect(host=config.url, port=config.port)
+    ftps.login(user=config.user, passwd=config.password)
+    ftps.prot_c()
+    ftps.cwd(f'/in_file/00/logs/rahsam/{time.strftime("%Y/%m/%Y%m09")}')
 
-        for terminal in CSVReader.get_terminals():
-            try:
-                ftps.cwd(terminal)
-                if len(ftps.nlst()) == 0:
-                    print(terminal + ' is empty')
-                elif len(ftps.nlst()) > 1:
-                    print(terminal + ' more than one file')
-                else:
-                    print(terminal + ' is OK')
-            except ftplib.error_perm:
-                print(terminal + ' not found')
+    for terminal in CSVReader.get_terminals():
+        try:
+            ftps.cwd(terminal)
+            if len(ftps.nlst()) == 0:
+                print(terminal + ' is empty')
+            elif len(ftps.nlst()) > 1:
+                print(terminal + ' more than one file')
             else:
-                ftps.cwd('..')
+                print(terminal + ' is OK')
+        except ftplib.error_perm:
+            print(terminal + ' not found')
+        else:
+            ftps.cwd('..')
